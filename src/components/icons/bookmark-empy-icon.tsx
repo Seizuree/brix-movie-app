3468b0386e9b81d48fd611255977048d7fc5eb09
Brian <briangiovanni03@gmail.com>
2024-03-07 16:29:00 +0700

    Initial commit

---
+++ src/components/icons/bookmark-empy-icon.tsx
@@ -0,0 +1,27 @@
+const BookmarkEmptyIcon = () => {
+  return (
+    <>
+      <svg
+        width="12"
+        height="14"
+        viewBox="0 0 12 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_1_3)">
+          <path
+            d="M10.9818 0.352075L10.9818 12.8272L5.8622 8.31829L0.889805 12.8272L0.889805 0.291789L10.9818 0.352075Z"
+            stroke="white"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_1_3">
+            <rect width="12" height="14" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </>
+  )
+}
+
+export default BookmarkEmptyIcon
